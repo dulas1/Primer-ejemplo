baae143e17db588b8543745ea99960b23bc52d27
dulas1 <ti13110192@itess.edu.mx>
2017-08-04 11:12:43 -0500

    Creo un ejemplo que arroja un mensaje
    Esto es un ejemplo que lanza un mensaje al presionar un boton.
    el mensaje dice -Esto es una prueba-

---
+++ Ejemplo1/Ejemplo1/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+        MessageBox.Show("Esto es una prueba", "Mensage")
+
+    End Sub
+End Class

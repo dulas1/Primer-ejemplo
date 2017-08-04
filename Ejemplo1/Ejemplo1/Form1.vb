d26cde6060e24308bcbdd780dad146e4057c2acb
dulas1 <ti13110192@itess.edu.mx>
2017-08-04 12:21:45 -0500

    Ingresar texto
    Se agrego un boton, caja de texto, y una lista de texto, la cual agrega el texto y se visualiza.

--- Ejemplo1/Ejemplo1/Form1.vb
+++ Ejemplo1/Ejemplo1/Form1.vb
@@ -6,4 +6,11 @@
         MessageBox.Show("Esto es una prueba", "Mensage")
         MessageBox.Show(primer, "¡Hola a todos!")
     End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+
+        lbox1.Items.Add(txt1.Text)
+
+    End Sub
+
 End Class

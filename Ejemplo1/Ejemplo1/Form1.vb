506f0654e50bee7fc32ed2805152d8606a02b478
dulas1 <ti13110192@itess.edu.mx>
2017-08-04 11:55:57 -0500

    agrege otro mensaje emergente
    alguien agrego un nuevo mensaje, con el uso de una variable

--- Ejemplo1/Ejemplo1/Form1.vb
+++ Ejemplo1/Ejemplo1/Form1.vb
@@ -1,7 +1,9 @@
 ﻿Public Class Form1
+
+    Dim primer As String = "Hola Mundo"
     Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
 
         MessageBox.Show("Esto es una prueba", "Mensage")
-
+        MessageBox.Show(primer, "¡Hola a todos!")
     End Sub
 End Class

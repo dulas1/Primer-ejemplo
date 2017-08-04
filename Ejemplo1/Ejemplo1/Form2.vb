cfd34074ecf19829c2df69ce2fcea8914aa4fee0
dulas1 <ti13110192@itess.edu.mx>
2017-08-04 14:06:55 -0500

    nuevo boton

---
+++ Ejemplo1/Ejemplo1/Form2.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form2
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+        MessageBox.Show("esta es el sugundo formulario")
+    End Sub
+End Class
